0b9a5ca6d580832d85ef8fff618770d2570e8165
kabirdutt0907 <NANAK@#dutt09>
2022-05-30 08:43:17 +0530

    Finished Day-2

--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+projectList = [
+    {
+        'id':'1',
+        'title':"Ecommerce Website",
+        'description':'Fully Functional ecommerce Website'
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': 'This was a project where I built out my portfolio website'
+    },
+    {
+        'id': '3',
+        'title': "Social Network",
+        'description': 'Amazing open source project'
+    },
+
+]
+
+
+
 
 def projects(request):
-    return render(request , 'projects/projects.html')
+    page = 'Projects'
+    number = 20
+    context = {'Apage': page, 'Number': number, 'data': projectList}
+    return render(request , 'projects/projects.html', context)
 
 
 def project(request , pk):
-    return render(request , 'projects/single-project.html')
+    ProjectObj = None
+    for i in projectList:
+        if(i['id'] == pk):
+            ProjectObj = i
+    return render(request , 'projects/single-project.html' , {'ProjectObj' : ProjectObj})
